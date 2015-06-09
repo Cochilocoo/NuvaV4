21db1a477aca64ca2141d9cb57407d74980a10ce
Cochilocoo <a.peruyero@hotmail.com>
2015-06-09 01:22:26 -0500

    modificacion de servicios

--- js/script.js
+++ js/script.js
@@ -2,3 +2,50 @@ $(document).ready(function(){
 	"use strict";
 
 });
+/*
+$('#infoWeb.infoService').show();
+
+$('.serviceMenu').on('click', function(){
+    var indx = $(this).index();
+    $('.infoService div').hide();
+    $('.infoService div').eq(indx+4).show();
+})
+*/
+
+$('#infoWeb.infoService').show();
+
+$('#webDesign').on('click', function(){
+    $('#infoWeb').show();
+    $('#infoApp').hide();
+    $('#infoSoc').hide();
+    $('#infoBra').hide();
+    $('#infoPho').hide();
+})
+$('#appDev').on('click', function(){
+    $('#infoWeb').hide();
+    $('#infoApp').show();
+    $('#infoSoc').hide();
+    $('#infoBra').hide();
+    $('#infoPho').hide();
+})
+$('#socialMedia').on('click', function(){
+    $('#infoWeb').hide()
+    $('#infoApp').hide();
+    $('#infoSoc').show();
+    $('#infoBra').hide();
+    $('#infoPho').hide();
+})
+$('#branding').on('click', function(){
+    $('#infoWeb').hide()
+    $('#infoApp').hide();
+    $('#infoSoc').hide();
+    $('#infoBra').show();
+    $('#infoPho').hide();
+})
+$('#photo').on('click', function(){
+    $('#infoWeb').hide()
+    $('#infoApp').hide();
+    $('#infoSoc').hide();
+    $('#infoBra').hide();
+    $('#infoPho').show();
+})
